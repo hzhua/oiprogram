869b1168853dbc3304e74649797a98d1498a03fa
hzhua <hzhua201@gmail.com>
2009-03-03 23:31:47 +0800

    .

---
+++ DC/dc.cpp
@@ -0,0 +1,43 @@
+#define maxn 1000001
+#include<stdio.h>
+int wa[maxn],wb[maxn],wv[maxn],ws[maxn];
+int cmp(int *r,int a,int b,int l)
+{return r[a]==r[b]&&r[a+l]==r[b+l];}
+void print(int *arr,int n)
+	{
+	for(int i=0;i<=n;i++)printf("%d ",arr[i]);
+	printf("\n");
+	}
+void da(int *r,int *sa,int n,int m)
+{
+     int i,j,p,*x=wa,*y=wb,*t;
+     for(i=0;i<m;i++) ws[i]=0;
+     for(i=0;i<n;i++) ws[x[i]=r[i]]++;
+     for(i=1;i<m;i++) ws[i]+=ws[i-1];
+     for(i=n-1;i>=0;i--) sa[--ws[x[i]]]=i;
+     for(j=1,p=1;p<n;j*=2,m=p)
+     {
+       for(p=0,i=n-j;i<n;i++) y[p++]=i;
+       print(y,n-1);
+       for(i=0;i<n;i++) if(sa[i]>=j) y[p++]=sa[i]-j;
+       print(y,n-1);
+       for(i=0;i<n;i++) wv[i]=x[y[i]];
+       for(i=0;i<m;i++) ws[i]=0;
+       for(i=0;i<n;i++) ws[wv[i]]++;
+       for(i=1;i<m;i++) ws[i]+=ws[i-1];
+       for(i=n-1;i>=0;i--) sa[--ws[wv[i]]]=y[i];
+       print(sa,n-1);
+       for(t=x,x=y,y=t,p=1,x[sa[0]]=0,i=1;i<n;i++)
+       x[sa[i]]=cmp(y,sa[i-1],sa[i],j)?p-1:p++;
+     }
+     return;
+}
+int main()
+	{
+	int sa[100];
+	int r[100]={4,6,1,7,3,2};
+	da(r,sa,6,8);
+	for(int i=0;i<6;i++)
+		printf("%d ",sa[i]);
+	return 0;
+	}
